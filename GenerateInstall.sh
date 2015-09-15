1cc6044e8590809dd38883c7124201a06f8b6baa
Cashey <635976510@qq.com>
2015-09-16 00:14:52 +0800

    bug fix for GenerateInstall.sh

--- GenerateInstall.sh
+++ GenerateInstall.sh
@@ -7,12 +7,16 @@ SRC_DIR="${HOME_DIR}/src"
 mkdir -p ${INSTALLER_DIR}
 
 cd ${SRC_DIR} \
-&& tar zcvf vimrc.tgz .vim .vimrc \
-&& tar zcvf ${INSTALLER_DIR}/cashey_vimrc_install.tgz vimrc.tgz Install.sh
+&& tar zcf vimrc.tgz .vim .vimrc \
+&& mkdir -p cashey_vimrc \
+&& cp vimrc.tgz Install.sh cashey_vimrc \
+&& tar zcf ${INSTALLER_DIR}/cashey_vimrc_install.tgz cashey_vimrc \
+&& rm -rf cashey_vimrc
 
 if [[ $? -eq 0 ]]
 then
-    echo "Installer successfully packed."
+    echo "Installer successfully packed:"
+    echo "  ${INSTALLER_DIR}/cashey_vimrc_install.tgz"
 else
     echo "Installer pack failed."
 fi
