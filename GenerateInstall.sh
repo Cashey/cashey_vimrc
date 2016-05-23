d93c945aeaa35c6f8b4b4f6309ad68c24d2332e2
DeepinW <635976510@qq.com>
2016-05-23 18:06:39 +0800

    Update GenerateInstall.sh

--- GenerateInstall.sh
+++ GenerateInstall.sh
@@ -8,15 +8,15 @@ mkdir -p ${INSTALLER_DIR}
 
 cd ${SRC_DIR} \
 && tar zcf vimrc.tgz .vim .vimrc \
-&& mkdir -p cashey_vimrc \
-&& cp vimrc.tgz Install.sh cashey_vimrc \
-&& tar zcf ${INSTALLER_DIR}/cashey_vimrc_install.tgz cashey_vimrc \
-&& rm -rf cashey_vimrc
+&& mkdir -p install_vimrc \
+&& cp vimrc.tgz Install.sh install_vimrc \
+&& tar zcf ${INSTALLER_DIR}/install_vimrc.tgz install_vimrc \
+&& rm -rf install_vimrc
 
 if [[ $? -eq 0 ]]
 then
     echo "Installer successfully packed:"
-    echo "  ${INSTALLER_DIR}/cashey_vimrc_install.tgz"
+    echo "  ${INSTALLER_DIR}/install_vimrc.tgz"
 else
     echo "Installer pack failed."
 fi
